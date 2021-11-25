3b73f7caa755a170e35b5d5da448bd74473f1d1b
subramanian.s <subramanian.s@inxsasia.com>
2021-11-25 09:34:15 +0530

    doc added

--- src/components/app.js
+++ src/components/app.js
@@ -79,31 +79,54 @@ class App extends React.Component {
         this.setState({ itemList: items });
     }
 
-    // Show/hide cart Modal window
-    toggleCart () {
-        this.setState({cartVisible: !this.state.cartVisible});
+	// Here, calculating the each and every selected items and create a package.
+    placeOrder() {
+        var cartItems = this.state.itemList.filter((e)=> (e.isChecked == true))
+        console.log('cartItems :', JSON.stringify(cartItems));
+
+        if(cartItems.length) {
+            var numOfPackage = this.findNumOfPackage(cartItems);
+            console.log('numOfPackage :', numOfPackage);
+
+            if(numOfPackage == 1) {
+                this.createCartList(cartItems, numOfPackage);
+            } else {
+                cartItems.sort((a, b) => parseInt(b.Weight) - parseInt(a.Weight));
+                console.log('cartItems :', JSON.stringify(cartItems));
+
+                var Package = [];
+                cartItems.forEach((val, index) => {
+                    if(index < numOfPackage) {
+                        for(var i=0; i<numOfPackage; i++) {
+                            if(i == index) {
+                                Package[i] = [];
+                                Package[i].push(val);
+                                return;
+                            }
+                        }
+                    } else {
+                        Package.sort(this.sortFunction);
+                        for(var i=0; i<numOfPackage; i++) {
+                            var TPRIZE = 0;
+                            for(var j=0; j<Package[i].length; j++) {
+                                TPRIZE += Package[i][j].Price;
+                            }
+
+                            if (TPRIZE + val['Price'] <= maxOrderPrice) {
+                                Package[i].push(val);
+                                return;
+                            }
+                        }
+                    }
+                })
+
+                this.createCartList(Package, numOfPackage);
+            }
+        } else {
+            alert("choose any item to place order");
+        }
     }
 
-    // Clear/Reset selected items
-    clearCart () {
-        var data = this.state.itemList;
-        data.forEach((item, index, array) => {
-            if(item.isChecked)
-                this.toggleItem(index);
-        })
-        this.setState({ itemList: data, cartList: [], cartVisible: false });
-    }
-
-    // To find NumOfPackage while placing order
-    findNumOfPackage(cartItems) {
-        var totalPrice = 0;
-        cartItems.forEach((val, index) => {
-            totalPrice += val['Price'];
-        })
-        return Math.ceil(totalPrice/maxOrderPrice);
-    }
-
-    // To create a package array and show the cart popup
     createCartList(allItems, numOfPackage) {
         var data = [];
         if(numOfPackage == 1){
@@ -117,7 +140,7 @@ class App extends React.Component {
         this.toggleCart();
     }
 
-    // To form the package array which contains totalWeight, totalPrice and courierPrice
+	// To form the package array which contains totalWeight, totalPrice and courierPrice
     formPackage(allItems) {
         var items = '',totalWeight = 0,totalPrice = 0,courierPrice = 0;
         allItems.forEach((val, index) => {
@@ -134,13 +157,22 @@ class App extends React.Component {
         return {'items': items, 'tw': totalWeight, 'tp': totalPrice, 'cp': courierPrice};
     }
 
-    // To find courierPrice
+	// To find courierPrice
     findCourierCharge(g) {
         if(g <= 200) return '$5';
         else if(g > 200 && g <= 500) return '$10';
         else if(g > 500 && g <= 1000) return '$15';
         else if(g > 1000 && g <= 5000) return '$20';
         else return '$25';
+    }
+
+	// To find NumOfPackage while placing order
+    findNumOfPackage(cartItems) {
+        var totalPrice = 0;
+        cartItems.forEach((val, index) => {
+            totalPrice += val['Price'];
+        })
+        return Math.ceil(totalPrice/maxOrderPrice);
     }
 
 	// Sorting the array to get lower weight value as first
@@ -152,140 +184,20 @@ class App extends React.Component {
         }
     }
 
-    // To get overall weight
-    getWeight(packArray) {
-        var totalWeight = 0
-        packArray.forEach((val, index) => {
-            totalWeight += val.Weight
+	// Show/hide cart Modal window
+    toggleCart () {
+        this.setState({cartVisible: !this.state.cartVisible});
+    }
+
+	// Clear/Reset selected items
+    clearCart () {
+        var data = this.state.itemList;
+        data.forEach((item, index, array) => {
+            item.isChecked = false
         })
-        return totalWeight
+        this.setState({ itemList: data, cartList: [], cartVisible: false });
+
+
     }
-
-    // To get overall price
-    getPrice(packArray) {
-        var totalPrice = 0
-        packArray.forEach((val, index) => {
-            totalPrice += val.Price
-        })
-        return totalPrice
-    }
-
-
-    placeOrder() {
-        var cartItems = this.state.itemList.filter((e)=> (e.isChecked == true))
-        if(cartItems.length) {
-            var packagesArray = [];
-            var numOfPackage = this.findNumOfPackage(cartItems);
-            cartItems.sort((a, b) => parseInt(b.Price) - parseInt(a.Price));
-
-            for(var i=0; i<numOfPackage; i++) {
-                packagesArray[i] = [];
-                packagesArray[i].push(cartItems[i]);
-            }
-
-            cartItems = this.removeItemsFrom(cartItems, packagesArray);
-
-            packagesArray = this.getOrderedPackagesWithWeightLimit(cartItems, packagesArray);
-
-            var nonAccomodateableItems = this.removeItemsFrom(cartItems, packagesArray);
-            for (var item of nonAccomodateableItems) {
-                packagesArray.push([item]);
-            }
-
-            console.log('packagesArray')
-            console.log(packagesArray)
-
-            this.createCartList(packagesArray, numOfPackage);
-        } else {
-            alert("choose any item to place order");
-        }
-    }
-
-    getOrderedPackagesWithWeightLimit(itemsArray, packagesArray) {
-        var packagesArray = packagesArray;
-
-        for(var i=0; i<itemsArray.length; i++) {
-            var weightArray = [];
-            // packagesArray.sort(this.sortFunction);
-            // packagesArray = packagesArray.sort((a, b) => parseInt(this.getWeight(a).Weight) - parseInt(this.getWeight(b).Weight));
-            for(var pack of packagesArray) {
-                weightArray.push(this.getWeight(pack));
-            }
-            var item = itemsArray[i];
-            var isInserted = false;
-
-            for(var j=0; j<weightArray.length; j++) {
-                if(!isInserted) {
-                    if((this.getPrice(packagesArray[j])+item.Price) <= maxOrderPrice) {
-                        packagesArray[j].push(item);
-                        isInserted = true;
-                    }
-                }
-            }
-        }
-        return packagesArray;
-    }
-
-    removeItemsFrom(itemsArray, packagesArray) {
-        var temArray = itemsArray;
-
-        for(var i=0; i<itemsArray.length; i++) {
-            for(var j=0; j<packagesArray.length; j++) {
-                var pack = packagesArray[j];
-                for(var tempItem of pack) {
-                    if(itemsArray[i].Name == tempItem.Name) {
-                        temArray = temArray.filter((e) => (e.Name != tempItem.Name))
-                    }
-                }
-            }
-        }
-        return temArray;
-    }
-
-
-    /*// Here, calculating the each and every selected items and create a package.
-        placeOrder() {
-            var cartItems = this.state.itemList.filter((e)=> (e.isChecked == true))
-
-            if(cartItems.length) {
-                var numOfPackage = this.findNumOfPackage(cartItems);
-
-                if(numOfPackage == 1) {
-                    this.createCartList(cartItems, numOfPackage);
-                } else {
-                    cartItems.sort((a, b) => parseInt(b.Weight) - parseInt(a.Weight));
-
-                    var Package = [];
-                    cartItems.forEach((val, index) => {
-                        if(index < numOfPackage) {
-                            for(var i=0; i<numOfPackage; i++) {
-                                if(i == index) {
-                                    Package[i] = [];
-                                    Package[i].push(val);
-                                    return;
-                                }
-                            }
-                        } else {
-                            Package.sort(this.sortFunction);
-                            for(var i=0; i<numOfPackage; i++) {
-                                var TPRIZE = 0;
-                                for(var j=0; j<Package[i].length; j++) {
-                                    TPRIZE += Package[i][j].Price;
-                                }
-
-                                if (TPRIZE + val['Price'] <= maxOrderPrice) {
-                                    Package[i].push(val);
-                                    return;
-                                }
-                            }
-                        }
-                    })
-
-                    this.createCartList(Package, numOfPackage);
-                }
-            } else {
-                alert("choose any item to place order");
-            }
-        }*/
 }
 export default App;
